7c8f6d0c69b722233c706eaa5fbd96d0c0eded39
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-02-08 16:13:13 +0530

    Frontend: Added basic landing page structure
    Added a form to enter location and a navbar.

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import './Form.css'
+
+export default function Form() {
+  return (
+    <div className='form-div'>
+        <form>
+            <label>
+                <input type="text" placeholder='E.g New York' />
+            </label>
+        </form>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+    return (
+        <div>
+        <nav>
+            <h1>Thunder</h1>
+            <div>
+                <ul>
+                    <li><a href="index.html">About</a></li>
+                    <li><a href="index.html">Live</a></li>
+                </ul>
+            </div>
+        </nav>
+        </div>
+    )
+}
+
